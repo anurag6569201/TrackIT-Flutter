376d70c3cad1b234cede50a2f6be3c5161bf0e7e
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2025-03-09 20:43:43 +0530

    updated with sanitas main widget

---
+++ sanitas/android/app/src/main/kotlin/com/example/sanitas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sanitas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ sanitas/lib/constants/strings.dart
@@ -0,0 +1,4 @@
+String groupId = 'group.home_widget_demo_group';
+String iosWidget = 'home_widget_demo';
+String androidWidget = 'CustomHomeView';
+String key = '0ecd5437269046193ac6acb87f964e0c';
